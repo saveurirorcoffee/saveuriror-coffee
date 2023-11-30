bc23afc4f4175418b7de9296568fc4947b4721a8
saveurirorcoffee <150459461+saveurirorcoffee@users.noreply.github.com>
2023-11-30 19:38:55 +0700

    Update card.jsx
    add extraButton

--- src/components/landingPages/card.jsx
+++ src/components/landingPages/card.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-const CardLandingPage = ({ image, title, content, price, link}) => {
+const CardLandingPage = ({ image, title, content, price, link, extraButton = {} }) => {
     return (
         <div className="col-md-6 col-lg-4 rounded">
             <div className="card bg-dark text-white p-0 pb-3 shadow mb-5" style={{ border: 0, textAlign: "left", height: "520px" }}>
@@ -9,7 +9,8 @@ const CardLandingPage = ({ image, title, content, price, link}) => {
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text" style={{height:"80px"}}>{content}</p>
                     <p className="card-text">{price}</p>
-                    <Link to={link} className='btn button-coffee mt-3'>Order Now</Link>
+                    <Link to={link} className='btn button-coffee mt-3 me-3'>Order Now</Link>
+                    {extraButton.link ? <Link to={extraButton.link} className='btn button-coffee mt-3'>{extraButton.label}</Link> : null}
                 </div>
             </div>
         </div>
