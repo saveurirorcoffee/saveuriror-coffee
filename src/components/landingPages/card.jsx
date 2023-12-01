e1728a9e410a35a6d19f1f3c2c8d62a2099cf255
saveurirorcoffee <150459461+saveurirorcoffee@users.noreply.github.com>
2023-12-01 14:13:52 +0700

    Update card.jsx
    fix image fit

--- src/components/landingPages/card.jsx
+++ src/components/landingPages/card.jsx
@@ -4,7 +4,9 @@ const CardLandingPage = ({ image, title, content, price, link, extraButton = {} 
     return (
         <div className="col-md-6 col-lg-4 rounded">
             <div className="card bg-dark text-white p-0 pb-3 shadow mb-5" style={{ border: 0, textAlign: "left", height: "520px" }}>
-                <img style={{ height: "200px" }} src={image} className="card-img-top" alt="..." />
+                <div style={{ height: "200px" }}>
+                    <img style={{ objectFit: "cover", height: "100%" }} src={image} className="card-img-top" alt="..." />
+                </div>
                 <div className="card-body p-4">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text" style={{height:"80px"}}>{content}</p>
