14e1b48abfc4cee14a5dc7d05733f4313225896d
saveurirorcoffee <150459461+saveurirorcoffee@users.noreply.github.com>
2023-12-05 11:12:07 +0700

    Update ourProduct.jsx

--- src/sections/landingPages/ourProduct.jsx
+++ src/sections/landingPages/ourProduct.jsx
@@ -12,7 +12,7 @@ const OurProductLandingPage = () => {
             />
             <CardLandingPage
                 image="https://th.bing.com/th/id/R.da3e3744b64a85a769a5c01c7f18e1cc?rik=QXmQtOuTwAHu7A&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fb%2fb5%2fMedium_roasted_Arabica_coffee_beans.jpg&ehk=Zf75HiIhszfmZwZa9dlaof9TGDTJ6RkldoCA9h%2fjq4g%3d&risl=1&pid=ImgRaw&r=0"
-                title="Arabica Coffee Roasted Beans"
+                title="<b></CardLandingPage>Arabica Coffee Roasted Beans</b></CardLandingPage>"
                 content="Unleash the bold flavors of meticulously roasted coffee beans, crafted for a rich and deep taste. Elevate your coffee experience sip by sip!"
                 price="Coming soon"
                 link="/404"
