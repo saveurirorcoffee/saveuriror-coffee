2c5c55f57705a0c6d32cd19e0e2e5e0ba4abdaf9
saveurirorcoffee <150459461+saveurirorcoffee@users.noreply.github.com>
2023-12-05 10:07:56 +0700

    Update ourProduct.jsx

--- src/sections/landingPages/ourProduct.jsx
+++ src/sections/landingPages/ourProduct.jsx
@@ -6,21 +6,21 @@ const OurProductLandingPage = () => {
             <CardLandingPage
                 image="https://th.bing.com/th/id/OIP.YapA0lZmKl3H0lKfvWTNVwHaG_?pid=ImgDet&rs=1"
                 title="Arabica Greenbeans Coffee"
-                content="Quality coffee beans that have not undergone the roasting process. Suitable for coffee shops that want to be partner"
+                content="Unroasted, premium-grade coffee beans crafted for perfection. Ideal for coffee shops seeking exceptional partnership opportunities. Unlock the true potential of your brew with these high-quality green coffee beans!"
                 price="Coming soon"
                 link="/404"
             />
             <CardLandingPage
                 image="https://th.bing.com/th/id/R.da3e3744b64a85a769a5c01c7f18e1cc?rik=QXmQtOuTwAHu7A&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fb%2fb5%2fMedium_roasted_Arabica_coffee_beans.jpg&ehk=Zf75HiIhszfmZwZa9dlaof9TGDTJ6RkldoCA9h%2fjq4g%3d&risl=1&pid=ImgRaw&r=0"
                 title="Arabica Coffee Roasted Beans"
-                content="Coffee beans that have gone through the roasting process, ready to deliver a rich and deep flavour."
+                content="Unlock the bold essence of roasted coffee beans, meticulously crafted to deliver a rich and deep flavor sensation. Ready to elevate your coffee experience with every sip."
                 price="Coming soon"
                 link="/404"
             />
             <CardLandingPage
                 image="images/product.jpeg"
-                title="Pangalengan Ground Arabica Coffee - Saveurior Coffee - Varian Original"
-                content="Ready-to-brew coffee variants provide convenience i n enjoying the distinctive flavour of coffee."
+                title="Arabica Pangalengan Drip Coffee"
+                content="Enjoy the convenience of Arabica Drip Coffee, a premium blend from Pangalengan, West Java. Designed for easy brewing with its handy drip bag packaging. Just pour hot water over the drip bag for a rich and hassle-free coffee experience!"
                 price={<p><span style={{ textDecoration: "line-through" }}>Rp 58.000</span>&nbsp;<span>Rp 40.250</span></p>}
                 link="https://www.marketekspor.com/product/kopi-bubuk-pangalengan-saveurior-coffee-varian-original/"
                 extraButton={{ link: "https://shp.ee/drmmuic", label: "Order on Shopee" }}
