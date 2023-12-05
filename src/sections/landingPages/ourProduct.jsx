44e7f69e1f49ce381bdebb551669beef31368a2d
saveurirorcoffee <150459461+saveurirorcoffee@users.noreply.github.com>
2023-12-05 12:22:35 +0700

    Update ourProduct.jsx

--- src/sections/landingPages/ourProduct.jsx
+++ src/sections/landingPages/ourProduct.jsx
@@ -12,14 +12,14 @@ const OurProductLandingPage = () => {
             />
             <CardLandingPage
                 image="https://th.bing.com/th/id/R.da3e3744b64a85a769a5c01c7f18e1cc?rik=QXmQtOuTwAHu7A&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fb%2fb5%2fMedium_roasted_Arabica_coffee_beans.jpg&ehk=Zf75HiIhszfmZwZa9dlaof9TGDTJ6RkldoCA9h%2fjq4g%3d&risl=1&pid=ImgRaw&r=0"
-                title="<b>Arabica Coffee Roasted Beans<b>"
+                title="<b>Arabica Coffee Roasted Beans</b>"
                 content="Unleash the bold flavors of meticulously roasted coffee beans, crafted for a rich and deep taste. Elevate your coffee experience sip by sip!"
                 price="Coming soon"
                 link="/404"
             />
             <CardLandingPage
                 image="images/product.jpeg"
-                title="Arabica Pangalengan Drip Coffee"
+                title=<b>"Arabica Pangalengan Drip Coffee"</b>
                 content="Enjoy the convenience of Arabica Drip Coffee with its handy drip bag packaging. Just pour hot water over the drip bag for a rich and hassle-free coffee experience!"
                 price={<p><span style={{ textDecoration: "line-through" }}>Rp 58.000</span>&nbsp;<span>Rp 40.250</span></p>}
                 link="https://www.marketekspor.com/product/kopi-bubuk-pangalengan-saveurior-coffee-varian-original/"
